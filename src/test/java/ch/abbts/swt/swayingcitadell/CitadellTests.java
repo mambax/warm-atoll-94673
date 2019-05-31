d3d8d0ebc2b3cd941cce0c8f1125506057e8a2f2
Dominik Meyer <d.meyer@gravity.global>
2019-05-31 09:27:59 +0200

    skeleton 3

--- src/test/java/ch/abbts/swt/swayingcitadell/CitadellTests.java
+++ src/test/java/ch/abbts/swt/swayingcitadell/CitadellTests.java
@@ -1,4 +1,4 @@
-package com.example.swayingcitadell;
+package ch.abbts.swt.swayingcitadell;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class CitadellTests {
 
 	@Test
 	public void contextLoads() {
