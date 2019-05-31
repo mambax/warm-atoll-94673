69952fdc33a97dae88a42392dbaadcb9577c9f00
Dominik Meyer <d.meyer@gravity.global>
2019-05-31 09:19:30 +0200

    skeleton

---
+++ src/main/ch/abbts/swt/Citadell.java
@@ -0,0 +1,21 @@
+package ch.abbts.swt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Citadell {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello Citadell!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Citadell.class, args);
+    }
+}
