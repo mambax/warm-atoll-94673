e1c9c6d1e59810fa6a5186f602bb720601b8c0ea
Dominik Meyer <d.meyer@gravity.global>
2019-05-31 10:26:16 +0200

    skeleton 4 with test

---
+++ src/main/java/ch/abbts/swt/MathController.java
@@ -0,0 +1,15 @@
+package ch.abbts.swt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("math")
+public class MathController {
+    @GetMapping("add")
+    public Integer add(@RequestParam(name = "summand1", defaultValue = "1") Integer s1, @RequestParam(name = "summand2", defaultValue = "1") Integer s2) {
+        return s1+s2;
+    }
+}
---
+++ src/test/java/ch/abbts/swt/swayingcitadell/MathControllerE2ETests.java
@@ -0,0 +1,28 @@
+package ch.abbts.swt.swayingcitadell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MathControllerE2ETests {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void testAdd() throws Exception {
+        this.webClient
+                .get()
+                .uri("/math/add?summand1=2&summand2=2")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Integer.class)
+                .isEqualTo(4);
+    }
+}
