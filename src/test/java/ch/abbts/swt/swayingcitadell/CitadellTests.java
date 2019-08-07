7924d324803cce7f92bf93afb0e2d78e05d25cce
dom <dominik.meyer01@sap.com>
2019-08-07 22:54:09 +0200

    Add alert endpoint

--- src/test/java/ch/abbts/swt/swayingcitadell/CitadellTests.java
+++ src/test/java/ch/abbts/swt/swayingcitadell/CitadellTests.java
@@ -2,15 +2,27 @@ package ch.abbts.swt.swayingcitadell;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CitadellTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private WebTestClient webClient;
 
+  @Test
+  public void testAlert() throws Exception {
+    this.webClient
+        .get()
+        .uri("/alert")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(String.class)
+        .isEqualTo("An alert has been put to the logs!");
+  }
 }
