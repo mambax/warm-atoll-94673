cabd7d3d309d0900574512c23575df23f190fda7
dom <dominik.meyer01@sap.com>
2020-06-19 15:03:34 +0200

    Mockito test for jonas

--- src/test/java/ch/abbts/swt/swayingcitadell/MathControllerTests.java
+++ src/test/java/ch/abbts/swt/swayingcitadell/MathControllerTests.java
@@ -3,6 +3,7 @@ package ch.abbts.swt.swayingcitadell;
 import ch.abbts.swt.MathController;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,7 +18,9 @@ public class MathControllerTests {
     @Test
     public void addTwoAndTwo() {
         Integer expected = 4;
-        Integer actual = mC.add(2, 2);
+        MathController mcSpy = Mockito.spy(mC);
+        Integer actual = mcSpy.add(2, 2);
         assertEquals(expected,actual);
+        Mockito.verify(mcSpy, Mockito.times(1)).add(2,2);
     }
 }
